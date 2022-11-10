ed4404408b7695880cf0da435495d395add2a88c
Nhut Huynh <67490369+nhuthlh@users.noreply.github.com>
2022-11-10 11:39:53 +0700

    Update index.tsx
    v3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.less";
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Welcome to My Page React sample CICD v2</h1>
+      <h1 className={styles.title}>Welcome to My Page React sample CICD v3</h1>
     </div>
   );
 }
